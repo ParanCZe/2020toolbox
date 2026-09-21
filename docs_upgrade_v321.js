5f8ebcadeb29b6e04653c468e698877060753d16
ParanCZe <96579738+ParanCZe@users.noreply.github.com>
2026-09-21 09:32:40 +0200

    Bump Upscaler and pricing caches

--- docs_upgrade_v321.js
+++ docs_upgrade_v321.js
@@ -74,11 +74,11 @@
   function loadPresentationLayering(){loadScriptOnce('presentation_layering_v357.js?v=357','presentation-layering-v357')}
   function loadPresentationGeometryCore(){loadScriptOnce('presentation_geometry_v358_core.js?v=3582','presentation-geometry-v358-core')}
   function loadPresentationGeometry(){loadScriptOnce('presentation_editor_geometry_v358.js?v=3583','presentation-editor-geometry-v358')}
-  function loadUpscaler(){loadScriptOnce('upscaler_v359.js?v=370','upscaler-v359')}
+  function loadUpscaler(){loadScriptOnce('upscaler_v359.js?v=371','upscaler-v359')}
   function loadCompareVisual(){loadScriptOnce('docs_compare_visual_v340.js?v=341','docs-compare-visual-v340')}
   function loadReleaseUI(){loadScriptOnce('release_ui_v341.js?v=354','release-ui-v341')}
   function loadDrawingColumns(){loadScriptOnce('docs_drawing_columns_v342.js?v=447','docs-drawing-columns-v342')}
-  function loadPricingOffer(){loadScriptOnce('pricing_offer_v345.js?v=349','pricing-offer-v349')}
+  function loadPricingOffer(){loadScriptOnce('pricing_offer_v345.js?v=350','pricing-offer-v349')}
   function loadPricingProfessionTemplate(){loadScriptOnce('pricing_profession_template_v349.js?v=349','pricing-profession-template-v349')}
   function loadPricingRowRates(){loadScriptOnce('pricing_row_rates_v352.js?v=352','pricing-row-rates-v352')}
   function loadPricingLayout(){loadScriptOnce('pricing_layout_v353.js?v=356fix','pricing-layout-v353')}
