15797f0abc727cc1fc99203c1de47e97d8390203
ParanCZe <96579738+ParanCZe@users.noreply.github.com>
2026-09-23 09:49:34 +0200

    Bump Texture Library to 0.1.9

--- SketchUpPlugins/TextureLibrarySource/twentytwenty_texture_library.rb
+++ SketchUpPlugins/TextureLibrarySource/twentytwenty_texture_library.rb
@@ -9,8 +9,8 @@ module TwentyTwenty
       '20-20 Texture Library',
       'twentytwenty_texture_library/main'
     )
-    EXTENSION.description = 'Texture library with SIKO, EGGER, Texture Club category workflow, per-face positioning, RAL and NCS.'
-    EXTENSION.version = '0.1.8'
+    EXTENSION.description = 'Texture library with SIKO, EGGER, in-SketchUp Texture Club live browser, per-face positioning, RAL and NCS.'
+    EXTENSION.version = '0.1.9'
     EXTENSION.creator = '20-20'
     Sketchup.register_extension(EXTENSION, true)
   end
