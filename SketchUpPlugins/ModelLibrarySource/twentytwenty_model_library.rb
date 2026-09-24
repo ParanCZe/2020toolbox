aa240b0345312f2e9f54eb24e2921ff062e20b3f
ParanCZe <96579738+ParanCZe@users.noreply.github.com>
2026-09-24 13:35:07 +0200

    Bump Model Library to v0.2.0

--- SketchUpPlugins/ModelLibrarySource/twentytwenty_model_library.rb
+++ SketchUpPlugins/ModelLibrarySource/twentytwenty_model_library.rb
@@ -7,10 +7,10 @@ module TwentyTwenty
   module ModelLibrary
     EXTENSION ||= SketchupExtension.new(
       '20-20 Model Library',
-      'twentytwenty_model_library/main'
+      'twentytwenty_model_library/main_v020'
     )
-    EXTENSION.description = 'Smart SketchUp model library with placement metadata for wall and ground assets.'
-    EXTENSION.version = '0.1.1'
+    EXTENSION.description = 'Parametric SketchUp object library with electrical devices, 2D vegetation, windows, doors and hosted cut-opening objects.'
+    EXTENSION.version = '0.2.0'
     EXTENSION.creator = '20-20'
     Sketchup.register_extension(EXTENSION, true)
   end
