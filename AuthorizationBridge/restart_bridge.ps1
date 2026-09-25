a594e2aa445c4c6c00b9ed403eebf451d5b16dfd
ParanCZe <96579738+ParanCZe@users.noreply.github.com>
2026-09-25 11:36:02 +0200

    AuthorizationBridge restart: expect 2.1.3

--- AuthorizationBridge/restart_bridge.ps1
+++ AuthorizationBridge/restart_bridge.ps1
@@ -2,7 +2,7 @@ param(
     [string]$PythonExe,
     [string]$BridgeScript,
     [string]$LogFile,
-    [string]$ExpectedVersion = "2.1.2"
+    [string]$ExpectedVersion = "2.1.3"
 )
 
 $ErrorActionPreference = 'Stop'
